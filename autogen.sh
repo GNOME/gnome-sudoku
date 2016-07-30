28260999d4b24039d40550dff76ecf56f73963b1
Michael Catanzaro <mcatanzaro@gnome.org>
2016-07-30 10:42:09 -0500

    Fix autogen with srcdir != builddir

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
 which gnome-autogen.sh || {
     echo "You need to install gnome-common from GNOME git (or from"
     echo "your OS vendor's package manager)."
