GNOME/gnome-sudoku

commit 3db33eb2d81dd674fe2fdb8205f0ce48e79ae9f4

Place numbers in number picker as on numpad
Now that it's common to open a number picker and then press a number on
the numpad, it's very annoying that the placement of the numbers don't
correspond properly:  This makes it easy to get confused, because you
look at the number to press on the number picker, but you need to press
a key that is located differently on your keyboard.

src/number-picker.vala
@@ -42,7 +42,7 @@ private class NumberPicker : Gtk.Grid
         {
             for (var row = 0; row < board.block_rows; row++)
             {
-                int n = col + row * board.block_cols + 1;
+                int n = col + (2 - row) * board.block_cols + 1;
 
                 var button = earmark ? new ToggleButton () : new Button ();
                 button.focus_on_click = false;
@@ -124,8 +124,7 @@ private class NumberPicker : Gtk.Grid
     {
         if (state && earmarks_active >= EARMARKS_MAX_ALLOWED)
             return false;
-        var button = (ToggleButton) this.get_child_at (index % board.block_cols, index / board.block_rows);
-        button.set_active (state);
+        get_button_for(index).set_active (state);
         return true;
     }
 
@@ -133,13 +132,19 @@ private class NumberPicker : Gtk.Grid
     {
         if (state)
             for (var i = 0; i < board.max_val; i++)
-                this.get_child_at (i % board.block_cols, i / board.block_rows).sensitive = true;
+                get_button_for(i).sensitive = true;
         else
             for (var i = 0; i < board.max_val; i++)
             {
-                var button = (ToggleButton) this.get_child_at (i % board.block_cols, i / board.block_rows);
+                var button = get_button_for (i);
                 if (!button.active)
                     button.sensitive = false;
             }
+    }
+
+    private ToggleButton get_button_for (int number)
+    {
+        return (ToggleButton) this.get_child_at(number % board.block_cols,
+            2 - (number / board.block_rows));
     }
 }

commit a1b5e23eec0689f6fe271397010466622b764871

number-picker: remove assumption that game is 3x3
This sort of broke in 3db33eb2d81dd674fe2fdb8205f0ce48e79ae9f4. Although
Sudoku does not support anything besides 3x3 games, the code is designed
to ensure it should work in theory. Let's avoid a 3x3 assumption that
snuck in when inverting the rows in the number picker.

src/number-picker.vala
@@ -45,7 +45,7 @@ private class NumberPicker : Grid
         {
             for (var row = 0; row < board.block_rows; row++)
             {
-                int n = col + (2 - row) * board.block_cols + 1;
+                int n = col + ((board.block_rows - 1) - row) * board.block_cols + 1;
 
                 var button = earmark ? new ToggleButton () : new Button ();
                 button.focus_on_click = false;
@@ -144,6 +144,6 @@ private class NumberPicker : Grid
     private ToggleButton get_button_for (int number)
     {
         return (ToggleButton) this.get_child_at (number % board.block_cols,
-            2 - (number / board.block_rows));
+            (board.block_rows - 1) - (number / board.block_rows));
     }
 }