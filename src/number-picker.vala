GNOME/gnome-sudoku

commit f8b33033f6bafaaa3926dc8c1c4c3472e1c66330

Fix Ctrl+num not setting earmarks
When using the keyboard to set earmarks, we need to call
game.cell_changed() to actually trigger the cell to draw. Currently the
earmark picker manages this for us if it exists (by emitting the
NumberPicker::earmark_state_changed signal, which SudokuCell listens
for) but if there is no earmark picker, this step is forgotten.

Turns out there is also no need to call game.enable/disable_earmark if
the earmark picker exists, for the same reason.

Fixes #74

src/sudoku-cell.vala
@@ -274,13 +274,19 @@ private class SudokuCell : Widget
             if (want_earmark && game.mode == GameMode.PLAY)
             {
                 var new_state = !game.board.is_earmark_enabled (row, col, key);
-                if (new_state)
-                    game.enable_earmark (row, col, key);
-                else
-                    game.disable_earmark (row, col, key);
 
                 if (earmark_picker != null)
+                {
                     earmark_picker.set_earmark (row, col, key - 1, new_state);
+                }
+                else
+                {
+                    if (new_state)
+                        game.enable_earmark (row, col, key);
+                    else
+                        game.disable_earmark (row, col, key);
+                    this.game.cell_changed (row, col, value, value);
+                }
             }
             else
             {

commit 3a2f1c376ebb9d9ad1d3377bda0ba3ca5715ef12

Fix misuse of non-earmark number picker as an earmark picker
We cannot call NumberPicker.set_earmark() on a NumberPicker that is not
an earmark picker. This bug was introduced in
f8b33033f6bafaaa3926dc8c1c4c3472e1c66330. It results in a critical
warning when trying to add earmarks with Ctrl+Num after left-clicking on
a cell to open the number picker.

Also, add assertions to crash earlier if these methods are misused.

src/number-picker.vala
@@ -131,12 +131,14 @@ private class NumberPicker : Grid
     }
 
     public void set_earmarks (int row, int col)
+        requires (is_earmark)
     {
         for (var i = 0; i < board.max_val; i++)
             set_earmark (row, col, i, board.is_earmark_enabled (row, col, i + 1));
     }
 
     public void set_earmark (int row, int col, int index, bool state)
+        requires (is_earmark)
     {
         get_button_for (index).set_active (state);
     }

src/sudoku-cell.vala
@@ -275,7 +275,7 @@ private class SudokuCell : Widget
             {
                 var new_state = !game.board.is_earmark_enabled (row, col, key);
 
-                if (number_picker != null)
+                if (number_picker != null && number_picker.is_earmark)
                 {
                     number_picker.set_earmark (row, col, key - 1, new_state);
                 }