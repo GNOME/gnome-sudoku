GNOME/gnome-sudoku

commit f519383e86390a9b4da60a978c3d15a21dd59dc7

Fix number picker's clear button The number picker clear button was calling disable_all_earmarks twice which was messing up the stack

src/number-picker.vala
@@ -99,11 +99,12 @@ private class NumberPicker : Grid
 
         clear_button.clicked.connect ((this_button) => {
             value_picked (0);
-            earmark_state_changed (0, false);
 
             if (is_earmark_picker)
                 for (var i = 0; i < 9; i++)
                     earmark_buttons[i].set_active (false);
+
+            this.set_clear_button_enabled (false);
         });
 
         this.valign = Align.CENTER;

src/sudoku-cell.vala
@@ -381,10 +381,8 @@ private class SudokuCell : Widget
         if (val > 0)
             popover.popdown ();
         else
-        {
-            this.game.board.disable_all_earmarks (row, col);
             value_picker.set_clear_button_visibility (false);
-        }
+
         this.value = val;
     }
 
@@ -395,13 +393,9 @@ private class SudokuCell : Widget
             if (!this.game.board.is_earmark_enabled (row, col, num))
                 this.game.enable_earmark (row, col, num);
         }
-        else
+        else if (this.game.board.is_earmark_enabled (row, col, num))
         {
-            if (num == 0)
-                this.game.disable_all_earmarks (row, col);
-
-            else if (this.game.board.is_earmark_enabled (row, col, num))
-                this.game.disable_earmark (row, col, num);
+            this.game.disable_earmark (row, col, num);
         }
 
         earmark_picker.set_clear_button_enabled (this.game.board.has_earmarks (row, col));

commit a28af0159a07590a04c59644559d727ebac8d270

number-picker: Fix clear button getting stuck Bug introduced in f519383e86390a9b4da60a978c3d15a21dd59dc7

src/number-picker.vala
@@ -101,10 +101,14 @@ private class NumberPicker : Grid
             value_picked (0);
 
             if (is_earmark_picker)
+            {
                 for (var i = 0; i < 9; i++)
                     earmark_buttons[i].set_active (false);
+                this.set_clear_button_enabled (false);
+            }
+            else
+                this.set_clear_button_visibility (false);
 
-            this.set_clear_button_enabled (false);
         });
 
         this.valign = Align.CENTER;