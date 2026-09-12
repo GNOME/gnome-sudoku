326d5ee43f09f49eec7dbd8958d9304fecf52c64
Johan G. <johan.gay75@gmail.com>
2026-09-12 17:08:10 +0200

    Another attempt at fixing i18n

--- src/game_view.rs
+++ src/game_view.rs
@@ -164,6 +164,7 @@ use super::*;
             self.backend.set(backend.clone()).expect("Game View is already initialized");
             self.obj().set_size_request (window.width_request(), window.height_request());
             connect_cb_pref!(self, connect_pref_timer_notify, show_timer_cb);
+            self.setup_actions();
 
             let backwards_controller = GestureClick::new();
             backwards_controller.set_button(8);
@@ -580,7 +581,6 @@ use super::*;
         fn constructed(&self) {
             self.parent_constructed();
             self.grid_overlay.set_child(Some(&self.grid));
-            self.setup_actions();
 
             self.menu_button.imp().main_menu.connect_closed(glib::clone!(
                 #[weak(rename_to = game_view)] self,
--- src/window.rs
+++ src/window.rs
@@ -206,14 +206,7 @@ mod imp {
                 create_cb_entry!(self, "shortcuts-dialog", shortcuts_dialog_cb),
                 create_cb_entry!(self, "back", back_cb),
                 create_cb_entry!(self, "zoom-reset", zoom_reset),
-
-                gio::ActionEntryBuilder::new("about-dialog")
-                    .activate(glib::clone!(
-                            #[weak(rename_to = obj)] self,
-                            move |_, _, _|
-                            obj.about_dialog_cb()
-                    ))
-                .build()
+                create_cb_entry!(self, "about-dialog", about_dialog_cb),
             ];
             action_group.add_action_entries(actions);
 
